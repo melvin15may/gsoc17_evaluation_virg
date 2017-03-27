c6daa49fb9e505c04f5f92d247343ce049c77e96
Melvin Mathew <melvin15may@gmail.com>
2017-03-26 22:05:51 -0700

    created basic app

--- tutorialkafka/tutorialkafka/settings.py
+++ tutorialkafka/tutorialkafka/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+# Kafka config 
+KAFKA_BROKER_URL = "localhost:9092"
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -118,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
