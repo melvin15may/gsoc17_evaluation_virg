a8c42b1c981c5ccb7e2f04019aaa36b411f8b49d
Melvin Mathew <melvin15may@gmail.com>
2017-03-27 12:05:13 -0700

    created APIs to produce a message

--- tutorialkafka/tk/views.py
+++ tutorialkafka/tk/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from kafka import KafkaProducer
+from django.conf import settings
+
+def producer(request):
+	producer = KafkaProducer(bootstrap_servers=[settings.KAFKA_BROKER_URL])
+	producer.send('sample-topic',bytes(request.GET["message"]))
+	producer.flush()
+	return HttpResponse('Produced message.')
 
 
-def index(request):
-    return HttpResponse("Hello, world.")
+
+
+
