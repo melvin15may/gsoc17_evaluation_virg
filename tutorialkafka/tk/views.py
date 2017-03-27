6211838241ff8620f308371834dae8323bebe22a
Melvin Mathew <melvin15may@gmail.com>
2017-03-27 14:30:43 -0700

    added comments

--- tutorialkafka/tk/views.py
+++ tutorialkafka/tk/views.py
@@ -2,9 +2,13 @@ from django.http import HttpResponse
 from kafka import KafkaProducer
 from django.conf import settings
 
+# Create messages
+
 def producer(request):
 	producer = KafkaProducer(bootstrap_servers=[settings.KAFKA_BROKER_URL])
+	# Send message with topic 'simple-topic'
 	producer.send('sample-topic',bytes(request.GET["message"]))
+	# Send all async messages
 	producer.flush()
 	return HttpResponse('Produced message.')
 
